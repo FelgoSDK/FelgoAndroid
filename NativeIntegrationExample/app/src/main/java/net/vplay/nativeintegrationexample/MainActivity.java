66fb03a2689f94e8d6daa5e47a1c0b2980e60424
Chrisu <c.bartsch@gmx.at>
2018-09-26 08:26:40 +0200

    Version 2.18.1

---
+++ NativeIntegrationExample/app/src/main/java/net/vplay/nativeintegrationexample/MainActivity.java
@@ -0,0 +1,35 @@
+package net.vplay.nativeintegrationexample;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import net.vplay.ui.VPlayAndroidActivity;
+import net.vplay.ui.VPlayAndroidFragment;
+
+import java.io.IOException;
+
+public class MainActivity extends VPlayAndroidActivity {
+
+  public static final String FRAGMENT_TAG = "my_vp_fragment";
+
+  @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    // activity_main.xml loads QML fragment:
+    setContentView(R.layout.activity_main);
+
+    // show QML fragment via code: (uncomment FrameLayout in activity_main.xml)
+/*    try {
+      getFragmentManager().beginTransaction()
+          .replace(R.id.fragment_container, new VPlayAndroidFragment()
+                  .setQmlSource(getApplicationContext(), "qml/Main.qml"),
+              null)
+          .addToBackStack(null)
+          .commit();
+    } catch (IOException ex) {
+      // qml file not found
+      Log.w("MainActivity", "Could not load QML file", ex);
+    }*/
+  }
+}
