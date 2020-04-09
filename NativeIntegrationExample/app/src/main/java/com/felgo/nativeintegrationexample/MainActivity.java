1bb6beaedb40ccc027a1bd9c5ecb7faf969a354f
Chrisu <chrisu.bartsch@gmail.com>
2020-04-09 16:44:46 +0200

    Use new Felgo version in example project and update android project

--- NativeIntegrationExample/app/src/main/java/com/felgo/nativeintegrationexample/MainActivity.java
+++ NativeIntegrationExample/app/src/main/java/com/felgo/nativeintegrationexample/MainActivity.java
@@ -2,20 +2,28 @@ package com.felgo.nativeintegrationexample;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 
 import com.felgo.ui.FelgoAndroidActivity;
 import com.felgo.ui.FelgoAndroidFragment;
 
-import java.io.IOException;
+import org.qtproject.qt5.android.bindings.QtFragment;
 
 public class MainActivity extends FelgoAndroidActivity {
+
+  private FelgoAndroidFragment m_qtFragment;
 
   @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
     // activity_main.xml loads QML fragment:
     setContentView(R.layout.activity_main);
+
+    Toolbar toolbar = findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+
+    m_qtFragment = (FelgoAndroidFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_container);
 
     // show QML fragment via code: (uncomment FrameLayout in activity_main.xml)
 /*    try {
